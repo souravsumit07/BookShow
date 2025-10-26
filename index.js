11c2ca23047fb9bee45ae3bd979dec6eb06cf1bf
Sumit Sourav Das <41sumitdas@gmail.com>
2025-10-26 11:55:45 +0530

    Done initial setup fot Bookshow

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app=express()
+
+
+
+const port = process.env.PORT || 5000;
+
+app.get("/",(req,res) => {
+    res.send("Hello from server")
+})
+
+app.listen(port,() => {
+    console.log(`Server is running on http://localhost:${port}`);
+    
+})

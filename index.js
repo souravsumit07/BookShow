01612e07df4bf06bb85046bc1cba37f1321f889c
Sumit Sourav Das <41sumitdas@gmail.com>
2025-10-26 12:05:40 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv"
+import connectDb from './config/dbConfig.js';
+dotenv.config()
+
 const app=express()
-
-
 
 const port = process.env.PORT || 5000;
 
@@ -11,5 +13,5 @@ app.get("/",(req,res) => {
 
 app.listen(port,() => {
     console.log(`Server is running on http://localhost:${port}`);
-    
+    connectDb()
 })

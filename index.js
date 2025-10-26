68209c4f55a9ea2ff11e9cb6baf6f8d0b67fd082
Sumit Sourav Das <41sumitdas@gmail.com>
2025-10-26 21:03:17 +0530

    Modified indes.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import connectDb from './config/dbConfig.js';
 import userRouter from "./routes/user.routes.js";
+import movieRouter from "./routes/movie.routes.js";
 
 dotenv.config()
 
@@ -12,7 +13,8 @@ const port = process.env.PORT || 5000;
 app.get("/",(req,res) => {
     res.send("Hello from server")
 })
-app.use("/api/v1/User",userRouter)
+app.use("/api/v1/users",userRouter)
+app.use("/api/v1/movies",movieRouter)
 
 
 app.listen(port,() => {
